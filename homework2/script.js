ad0d6be182b8590e04ddd6c459151c80ca545161
bohdanahostynska <bohdana271@gmail.com>
2022-11-08 11:48:45 +0200

    folders

---
+++ homework2/script.js
@@ -0,0 +1,30 @@
+let userName = "Bohdana";
+console.log(userName);
+
+userName = "Daryna";
+console.log(userName);
+
+let userSurname = "Hostynska";
+console.log(userSurname);
+
+let userAge = "34";
+console.log(userAge);
+
+let shoppingList;
+shoppingList = "apples" + "  " + "potatoes" + " " + "milk";
+console.log(typeof shoppingList, shoppingList);
+
+const isUserOnline = true;
+
+let workerSalary = 2000;
+workerSalary = 2000;
+console.log(workerSalary);
+
+let mouseCoordinates;
+mouseCoordinates = 190;
+
+let firstVariable = "Hello world"
+console.log(firstVariable);
+
+const clientAge = 34;
+console.log(clientAge);
