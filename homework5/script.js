06ad77b4b8c704eae61dcf0d7daefede9495404a
bohdanahostynska <bohdana271@gmail.com>
2022-11-27 23:38:17 +0200

    a few lines

--- homework5/script.js
+++ homework5/script.js
@@ -1,106 +1,55 @@
-// const LOGIN = "ADMIN";
-// const PASSWORD = "123456";
 
-// const authorize = () =>{
-//     let userLogin;
-//     let userPassword;
-
-
-// do {
-//     userLogin = prompt(" Логін:");
-//  if (!userLogin){
-//     alert("Введіть ваш логін");
-//     continue;
-//  }
-
-//     userPassword = prompt("Пароль:");
-//  if (!userPassword){
-//     alert("Введіть ваш пароль");
-//     continue;
-//  }
-
-//  if (userPassword !== PASSWORD || userLogin !== LOGIN){
-//     alert("Ваш дані невірні. Будь ласка, перевірте їх і спробуйте ще раз");
-//     continue;
-//  }
-
-//  break;
-
-// } while(true);
-
-// alert("Ласкаво просимо!")
-// }
-
-// authorize (); 
-
-// const drawPyramid = (height) =>{
-//     for (let i =0; i < height; i++){
-//         let spaceBefore = '';
-//         let stars = '';
-
-//         for ( let j = 0; j < 2 * i + 1; j++) {
-//             stars += "*";
-//         }
-
-//         for (let k =0; k < height - i -1; k++){
-//             spaceBefore += " ";
-//         }
-//         console.log(spaceBefore  + stars);
-//     }
-
-// };
-
-// const desiredHeight = Number (prompt ("Якою повинна бути висота піраміди?"));
-// drawPyramid(desiredHeight)
 //HOMEWORK//
 
 //3//
 
-// const LOGIN = "ADMIN";
-// const PASSWORD = "123456";
+const LOGIN = "ADMIN";
+const PASSWORD = "123456";
 
-// const authorize = () => {
-//     let userLogin;
-//     let userPassword;
-//     let isAuthsuccess = false;
+const authorize = () => {
+    let userLogin;
+    let userPassword;
+    let isAuthsuccess = false;
+    let userAttempts =3;
 
 
-//     if (userPassword === PASSWORD || userLogin === LOGIN) {
-//         isAuthsuccess = true;
-//         alert("Welcome");
+    while (userAttempts !==0) {
+
+        userLogin = prompt(" Логін:");
+        if (!userLogin) {
+            alert("Введіть ваш логін");
+            continue;
+        }
+        userPassword = prompt("Пароль:");
+        if (!userPassword) {
+
+            alert("Введіть ваш пароль");
+            continue;
+        }
+
+        if (userPassword === PASSWORD || userLogin === LOGIN) {
+            isAuthsuccess = true;
+            alert("Welcome");
+            return;
+        
+        }
+        if(userAttempts-- ||(userPassword !== PASSWORD || userLogin !== LOGIN)){
+            isAuthsuccess = false;
+            alert('Ваш дані невірні. Будь ласка, перевірте їх і спробуйте ще раз.Залишилось спроб:'+ userAttempts);
+            continue;
+        }
+
+    }  
     
-//     }
-    
+    if(isAuthsuccess ){
+        alert("Вітаю!Ваші дані вірні!")
+    }else{
+        alert("У вас не залишилось більше спроб")
+    }
 
-//     let i;
+} 
 
-//     while (i=1, i<=3, i++) {
-
-//         console.log("i",i);
-
-//         userLogin = prompt(" Логін:");
-//         if (!userLogin) {
-//             alert("Введіть ваш логін");
-//             continue;
-//         }
-//         userPassword = prompt("Пароль:");
-//         if (!userPassword) {
-
-//             alert("Введіть ваш пароль");
-//             continue;
-//         }
-
-//         if(userPassword !== PASSWORD || userLogin !== LOGIN){
-//             isAuthsuccess = false;
-//             alert("Ваш дані невірні. Будь ласка, перевірте їх і спробуйте ще раз");
-//             continue;
-//         }
-
-//     }  
-
-// } 
-
-// authorize();
+authorize();
 
 //1//
 
