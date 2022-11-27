c623e4ea9a309f3215d488ae87742e6513e5f677
bohdanahostynska <bohdana271@gmail.com>
2022-11-27 23:04:27 +0200

    task

--- homework6/script.js
+++ homework6/script.js
@@ -54,47 +54,41 @@ const userRegistrationSurName = () => {
 const userRegistrationPassword = (userString) => {
 
     let userPassword;
-    let userAttempts = 3;
+    let isAuthSuccess = false;
+    let userAttempts=3;
 
   
        while(userAttempts !==0){  
 
         userPassword = prompt(" Введіть ваш пароль(не менше ,ніж 6 символів,які містять в собі символи різного реєстру):");
-        console.log(userPassword.toLowerCase() === userPassword);
-        console.log(userPassword.toUpperCase() === userPassword);
-        console.log(userPassword);
-        console.log(!isNaN(userPassword || userPassword.toLowerCase() === userPassword || (userPassword.toUpperCase() === userPassword)))
-       
 
-        if(!userPassword.split("").some(el => el.toUpperCase() === el)){
-            alert("Данні помилкові.Вел буква" )
-            userAttempts--
+        if (userPassword.split("").some(el => el.toUpperCase() === el) || (userPassword.split("").some(el => el.toLowerCase() === el)) ||(userPassword.split("").some(el => +el))){
+            alert("Вітаю!Реєстрація пройшла успішно!")
             return;
+         
         }
-
-        if(!userPassword.split("").some(el => el.toLowerCase() === el)){
-            alert("Данні помилкові.Мал буква")
-            userAttempts--
-            return;
-        }
-
-        if(!userPassword.split("").some(el => +el)){
-            alert("Данні помилкові.цифра")
-            userAttempts--
-            return;
-        }
-
-        if ((userAttempts === 0)){
-            alert("Данні помилкові.цифра")
-        }
-
-        alert("Реєстрація успішна")
+        if ( userPassword.length < 6){
+            alert("Замало символів")
+            continue;
+        } else if   (userAttempts-- ||(!userPassword.split("").some(el => el.toUpperCase() === el) || (!userPassword.split("").some(el => el.toLowerCase() === el)) ||(!userPassword.split("").some(el => +el)))){
+            alert(`Данні помилкові.`+ userAttempts);
+            continue;
+        } 
+        
+    
    
         return userPassword
     }
     
+if(isAuthSuccess){
+    alert("Вітаю!Реєстрація пройшла успішно!")
+}else{
+    alert("Спроби закінчилися")
+}
+
 
 }
+
 const resName = userRegistrationName();
 const resSurName = userRegistrationSurName();
 const resPassword = userRegistrationPassword();
