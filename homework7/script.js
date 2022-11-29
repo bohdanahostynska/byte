123c81900f07756ffdcf8baecdabcc6c60491892
bohdanahostynska <bohdana271@gmail.com>
2022-11-29 21:59:04 +0200

    hw

--- homework7/script.js
+++ homework7/script.js
@@ -1,25 +1,71 @@
 	
 //2//
 
-const salaries = {
-    John: parseInt("4300.00"),
-    Ann: parseInt("5700.40"),
-    Pete: parseInt("4900.95"),
-  };
+// const salaries = {
+//     John: parseFloat("4300.00"),
+//     Ann: parseFloat("5700.40"),
+//     Pete:parseFloat("4900.95"),
+//   };
   
 
-  const getSalariesTotal = (salaries) => {
-    let total = 0;
-    for (let employee in salaries) {
-      console.log( employee);
-      console.log( salaries[employee])
-      total += salaries[employee];
+//   const getSalariesTotal = (salaries) => {
+//     let total = 0;
+//     for (let employee in salaries) {
+//       console.log( employee);
+//       console.log(salaries [employee])
+//       total += (salaries [employee])
+//       parseFloat(total)
+//     }
+
+//     return parseFloat(' 14901.35.');
+   
+//   }
+  
+//   const totalResult = getSalariesTotal(salaries);
+//     console.log(`totalResult`, totalResult)
+
+//1//
+        
+let userTime = (prompt("Enter your time: "));
+
+
+let  userTask = prompt("Enter your task:");
+
+
+let getHour;
+let getTask;
+
+function makeTimeTable (getHour,getTask){
+
+    for (let i = 0; i > 1; i++){  
+   
+    if (isNaN(userTime)) {
+        alert("Enter your time(Numbers only!)");
+            
     }
 
-    return total;
-  }
-  
-  const totalResult = getSalariesTotal(salaries);
-  console.log(`totalResult`, totalResult)
-  
+    if(!userTask){
+        alert("Enter your task!")
+     }  
 
+    if(userTime || userTask){
+        alert(userTime + `:`+ userTask)
+        continue;
+    }else{
+        (userTime || userTask)
+    }
+
+     return {
+        getHour:userTime,
+        getTask:userTask,
+    
+     }
+     
+    }
+    alert(userTime + `:`+ userTask)
+    
+
+    }
+
+    console.log(userTime + `:`+ userTask)
+    makeTimeTable (getHour,getTask);
