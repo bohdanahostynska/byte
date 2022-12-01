a27dd82b5a4a1cc626960643cfe8ac31ab21cae7
bohdanahostynska <bohdana271@gmail.com>
2022-12-01 23:30:44 +0200

    task

--- homework8/script.js
+++ homework8/script.js
@@ -1,18 +1,88 @@
-const countTrue = (arr) => {
-    let value = 0;
+//1//
 
-    for (let boolean of arr) {
-        if (boolean  === true) {
-            value += boolean
-            console.log(boolean);
+// const countTrue = (arr) => {
+//     let value = 0;
+
+//     for (let boolean of arr) {
+//         if (boolean  === true) {
+//             value += boolean
+//             console.log(boolean);
+//         }
+//     }
+//     return value;
+// }
+
+// const result = countTrue([true, false, false, true, false]);
+// const result2= countTrue([false, false, false, false]);
+// const result3 = countTrue([]);
+// console.log(result);
+// console.log(result2);
+// console.log(result2)
+
+
+//2//
+
+
+// const getOccurrencesCount = (arr) => {
+//     let value = 0;
+   
+
+//     for (let elem of arr) {
+
+//         if (elem === 'a'){
+//             value += elem
+//             console.log(elem);
+//         }
+        
+//         if (elem === 'b'){
+//             value += elem
+//             console.log(elem);
+
+//         }
+        
+//         if(elem === 'v'){
+//             value += elem
+//             console.log(elem);
+//         }
+
+//     }
+
+//     return value;
+
+
+// }
+
+// const resultCount = getOccurrencesCount(["a", "v", "a", "b", "b"]);
+// console.log('resultCount',resultCount);
+
+const getOccurrencesCount = (arr) => {
+        let value = 0;
+       
+    
+        for (let elem of arr) {
+    
+            if ((elem === "apples") || (elem === "oranges") || (elem === "pears")){
+                value += elem
+                console.log(elem);
+            }
+    
         }
+    
+        return value;
+    
+    
     }
-    return value;
-}
+    
+    const getCount = getOccurrencesCount([
+        "apples",
+        "oranges",
+        "pears",
+        "pears",
+        "apples",
+        "oranges",
+        "oranges",
+        "pears",
+      ]);
 
-const result = countTrue([true, false, false, true, false]);
-const result2= countTrue([false, false, false, false]);
-const result3 = countTrue([]);
-console.log(result);
-console.log(result2);
-console.log(result2)
+    console.log('getCount', getCount);
+    
