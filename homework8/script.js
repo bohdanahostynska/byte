791f59fa09a0c5c4626740de7e31e84cdade3242
bohdanahostynska <bohdana271@gmail.com>
2022-12-06 22:03:49 +0200

    task

--- homework8/script.js
+++ homework8/script.js
@@ -27,38 +27,30 @@
 //     let getCount = []
 
 //     for (let elem of arr) {
-//    getCount[elem] = getCount[elem] ? getCount[elem] + 1 : 1;
-//     }
 
+//       if(!getCount[elem]){
+//        getCount[elem]=1;
+//        }else{
+//         getCount[elem] +=1;
+//     }
+//   }
 //   return getCount
 // }
 // const resultCount = getOccurrencesCount(["a", "v", "a", "b", "b"]);
 // console.log('resultCount',resultCount);
 
+    // const getCount = getOccurrencesCount([
+    //     "apples",
+    //     "oranges",
+    //     "pears",
+    //     "pears",
+    //     "apples",
+    //     "oranges",
+    //     "oranges",
+    //     "pears",
+    //   ]);
 
-
-// const getOccurrencesCount = (arr) => {
-//     let getCount = []
-
-//     for (let fruit of arr) {
-//    getCount[fruit] = getCount[fruit] ? getCount[fruit] + 1 : 1;
-//     }
-
-//   return getCount
-// }
-
-//     const getCount = getOccurrencesCount([
-//         "apples",
-//         "oranges",
-//         "pears",
-//         "pears",
-//         "apples",
-//         "oranges",
-//         "oranges",
-//         "pears",
-//       ]);
-
-//     console.log('getCount', getCount);
+    // console.log('getCount', getCount);
    
       
 
@@ -66,37 +58,37 @@
   
 // (1)
 
- const findExcess = (arr) =>{ 
+//  const findExcess = (arr) =>{ 
   
 
-    const arrayOfOdd = []
-    const arrayOfEven = []
+//     const arrayOfOdd = []
+//     const arrayOfEven = []
  
-     for (let number of arr){ 
-             if (  ( number % 2 == 0)) {
-                 console.log('number is even',number);
-             } else {
-                 arrayOfOdd.push (number);
-                 console.log('number is odd',number);
-             }
-             console.log(number);
+//      for (let number of arr){ 
+//              if (  ( number % 2 == 0)) {
+//                  console.log('number is even',number);
+//              } else {
+//                  arrayOfOdd.push (number);
+//                  console.log('number is odd',number);
+//              }
+//              console.log(number);
 
-               if (  !( number % 2 == 0)) {
-                     console.log('number is odd',number);
-                 } else {
-                     arrayOfEven.push (number);
-                     console.log('number is even',number);
-                 }
-                 console.log(number);
+//                if (  !( number % 2 == 0)) {
+//                      console.log('number is odd',number);
+//                  } else {
+//                      arrayOfEven.push (number);
+//                      console.log('number is even',number);
+//                  }
+//                  console.log(number);
 
-         } 
-         return  arrayOfEven
+//          } 
+//          return  arrayOfEven
  
-     }
+//      }
 
-     const evenArray = findExcess([0, 1, 2]);
+//      const evenArray = findExcess([0, 1, 2]);
     //  const evenArray = findExcess ([1, 2, 3]);
     //  const evenArray = findExcess ([2, 6, 8, 10, 3]);
     //  const evenArray = findExcess ([0, 0, 3, 0, 0]);
     //  const evenArray = findExcess ([1, 1, 0, 1, 1]);
-     console.log ('evenArray',evenArray)
+    //  console.log ('evenArray',evenArray)
