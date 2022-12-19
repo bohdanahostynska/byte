07bfa145eea9cf848b42f561810bc22ac71cba54
bohdanahostynska <bohdana271@gmail.com>
2022-12-19 23:06:16 +0200

    task

--- homework11/script.js
+++ homework11/script.js
@@ -1,57 +1,46 @@
+
+
 const products = [
     {
-      name: "Iphone 12",
-      brand: "Apple",
-      price: 3200000,
-      properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+        name: "Iphone 12",
+        brand: "Apple",
+        price: 3200000,
+        properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
     },
     {
-      name: "Galaxy S20 Ultra",
-      brand: "Samsung",
-      price: 2900000,
-      properties: ["120 hz screen", "Water resistance"],
+        name: "Galaxy S20 Ultra",
+        brand: "Samsung",
+        price: 2900000,
+        properties: ["120 hz screen", "Water resistance"],
     },
     {
-      name: "MI 9",
-      brand: "Xiaomi",
-      price: 1300000,
-      properties: ["Best price", "Pay less - get more!"],
+        name: "MI 9",
+        brand: "Xiaomi",
+        price: 1300000,
+        properties: ["Best price", "Pay less - get more!"],
     },
-  ]
+]
 
-  const createBlogArticle = (name, brand, price, properties) =>{
+const createListFromAray = (arr) => {
 
-    console.log( products);
+    const list = document.createElement("div");
+    const listItem = document.createElement("div");
 
-    console.dir( products);
-    const blogContainer = document.createElement ('div');
-    const nameElement = document.createElement ('h2');
-    console.dir( nameElement);
-    const brandElement = document.createElement ('h3');
-    const priceElement = document.createElement ('h4');
-    const propertiesContainer = document.createElement ('div');
-  
-    nameElement.innerText = name;
-   
-    brandElement.innerText = brand;
+    arr.forEach((item) => {
+        listItem.innerHTML += (`
+        <div>
+          <h2>${item.name}</h2>
+          <h3>${item.brand}</h3>
+          <h4>${item.price}</h4>
+        <ul>
+        ${item.properties.map(properties => `<li>${properties}</li>`).join('')}
+        </ul>
+        </div>
+        `);
 
-    priceElement.innerText = price;
+        list.appendChild(listItem);
+    });
 
-   properties.forEach((item) => {
-      const itemElement = document.createElement("p");
-      itemElement.innerText = item;
-      propertiesContainer.append(itemElement);
-      console.log(propertiesContainer);
-    })    
-
-  blogContainer.append(nameElement,brandElement, priceElement,propertiesContainer);
-  console.log(blogContainer);
-
-  return blogContainer;
+    document.body.append(list)
 }
-
-  const blogItemElement = createBlogArticle(products);
-  console.log( products);
-//   document.body.append(blogItemElement);
-
-
+createListFromAray(products)
