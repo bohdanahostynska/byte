5bc243d7b06418a10fc467e67f88f99a67c42788
bohdanahostynska <bohdana271@gmail.com>
2022-12-25 11:54:45 +0200

    task

--- homework12/script.js
+++ homework12/script.js
@@ -1,15 +1,11 @@
 
-const createTooltip = () =>{ 
 
-  const tool = document.getElementById("Tooltip");
   const button = document.querySelector('.button');
   const buttonCoords = button.getBoundingClientRect();
   console.log(`buttonCoords`, buttonCoords);
-  
 
-}
-
-
+  const tippy = document.createElement("div");
+  console.log('tippy',tippy)
 
 
-createTooltip();
+
