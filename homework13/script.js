b738f7de1f58471076578e681ff71e4df683cbc9
bohdanahostynska <bohdana271@gmail.com>
2023-01-10 19:51:02 +0200

    task

--- homework13/script.js
+++ homework13/script.js
@@ -1,38 +1,31 @@
-
- const sliderWrapper =  document.querySelector('#wrapper');
+const sliderWrapper = document.querySelector('#wrapper');
 const sliders = document.querySelectorAll('.slider');
 console.log(sliders);
- const nextbutton = document.querySelector('#button-right');
- const prevbutton = document.querySelector('#button-left')
- const maxSlides=sliders.length-1;
- let currentSlide =0;
+const nextbutton = document.querySelector('#button-right');
+const prevbutton = document.querySelector('#button-left')
+const maxSlides = sliders.length - 1;
+let currentSlide = 0;
 
-nextbutton.addEventListener('click',()=>{
-console.log('click')
+nextbutton.addEventListener('click', () => {
+  console.log('click')
 
-if(currentSlide < maxSlides){
-  currentSlide+=1;
-}else{
-  currentSlide=0;
-}
-  
-sliderWrapper.style.left = `-${890*currentSlide}px`;
+  if (currentSlide < maxSlides) {
+    currentSlide += 1;
+  } else {
+    currentSlide = 0;
+  }
+
+  sliderWrapper.style.left = `-${890*currentSlide}px`;
 })
 
-prevbutton.addEventListener('click',()=>{
+prevbutton.addEventListener('click', () => {
   console.log('click')
-  
-  if(currentSlide < 0){
-    currentSlide=maxSlides;
-  }else{
-    currentSlide-=1;
+
+  if (currentSlide < 0) {
+    currentSlide = maxSlides;
+  } else {
+    currentSlide -= 1;
   }
-    
+
   sliderWrapper.style.left = `-${890*currentSlide}px`;
-  })
-
-
-
-
-
-
+})
